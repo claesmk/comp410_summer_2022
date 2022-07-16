79f91262214de23a9f8354230a9c365260b415e9
mkc <claesmk@gmail.com>
2022-07-16 18:50:07 -0400

    Quick sample with presidio_analyzer

--- test_scan.py
+++ test_scan.py
@@ -2,13 +2,24 @@ import unittest
 import pdfplumber
 import os
 import re
-from scan import show_aggie_pride, scan_files
+from scan import show_aggie_pride, scan_files, get_file_text
 import spacy
+from presidio_analyzer import AnalyzerEngine, RecognizerRegistry, PatternRecognizer, Pattern
 from docx import Document
 from openpyxl import load_workbook
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 
 class ScanTests(unittest.TestCase):
+    # python -m spacy download en_core_web_lg
+    try:
+        nlp = spacy.load("en_core_web_lg")
+    except OSError:
+        from spacy.cli import download
+        download("en_core_web_lg")
+        nlp = spacy.load("en_core_web_lg")
+
     def test_aggie_pride(self):
         # get the slogans
         slogans = show_aggie_pride()
@@ -71,17 +82,9 @@ class ScanTests(unittest.TestCase):
             self.assertIn(f, scan)
 
     def test_name_recognition(self):
-        # python -m spacy download en_core_web_sm
-        try:
-            nlp = spacy.load("en_core_web_lg")
-        except OSError:
-            from spacy.cli import download
-            download("en_core_web_lg")
-            nlp = spacy.load("en_core_web_lg")
-
         # Create a sample string with a mix of things that look like they could be names
         names_string = 'Name: John Jones\nAddress: 123 John W Mitchell Drive\nNorth Carolina\nHarsh Mupali'
-        doc = nlp(names_string)
+        doc = self.nlp(names_string)
 
         # expected results
         # Be careful with addresses that look like a name - they are often detected incorrectly
@@ -92,6 +95,72 @@ class ScanTests(unittest.TestCase):
 
         for ent in doc.ents:
             self.assertEqual(expected[ent.text], ent.label_)
+
+    def test_scan_pii(self):
+        # accout_recognizer = PatternRecognizer(supported_entity="US_BANK_ACCOUNT_NUMBER",
+        #                                       deny_list=['3942-29992'])
+        # Define the regex pattern in a Presidio `Pattern` object:
+        # numbers_pattern = Pattern(name="numbers_pattern", regex="\d+", score=0.5)
+        #
+        # # Define the recognizer with one or more patterns
+        # number_recognizer = PatternRecognizer(supported_entity="NUMBER", patterns=[numbers_pattern])
+
+        account_pattern = Pattern(name='account_pattern', regex=r'\d{3,4}-\d{5}', score=0.9)
+        account_recognizer = PatternRecognizer(supported_entity='ACCOUNT_NUMBER', patterns=[account_pattern])
+
+        # address_recognizer = PatternRecognizer(supported_entity="STREET_ADDRESS",
+        #                                        deny_list=['123 John W Mitchell Drive'])
+
+        address_pattern = Pattern(name='address_pattern', regex=r'\d+ [A-Z][a-z]+ \w+', score=0.5)
+        address_recognizer = PatternRecognizer(supported_entity='STREET_ADDRESS', patterns=[address_pattern])
+
+        twitter_pattern = Pattern(name='twitter_pattern', regex=r'\B@\w+|at [A-Z]+\w*', score=0.5)
+        twitter_recognizer = PatternRecognizer(supported_entity='TWITTER_HANDLE', patterns=[twitter_pattern])
+
+        extra_names_pattern = Pattern(name='extra_names_pattern',
+                                      regex=r'Jaden|Smith|Yuri|Krityan|Tatum', score=0.5)
+        extra_names_recognizer = PatternRecognizer(supported_entity='EXTRA_PERSON', patterns=[extra_names_pattern])
+
+        credit_card_pattern = Pattern(name='credit_card_pattern', regex=r'\d{4}[- ]\d{4}[- ]\d{4}[- ]\d{4}', score=0.9)
+        cc_recognizer = PatternRecognizer(supported_entity='CREDIT_CARD_NUMBER', patterns=[credit_card_pattern])
+
+        registry = RecognizerRegistry()
+        registry.load_predefined_recognizers()
+
+        # Add the recognizer to the existing list of recognizers
+        registry.add_recognizer(account_recognizer)
+        registry.add_recognizer(address_recognizer)
+        registry.add_recognizer(twitter_recognizer)
+        registry.add_recognizer(extra_names_recognizer)
+        registry.add_recognizer(cc_recognizer)
+
+        # Set up analyzer with our updated recognizer registry
+        analyzer = AnalyzerEngine(registry=registry)
+
+        # Detect types
+        detect_types = ['ACCOUNT_NUMBER', 'STREET_ADDRESS', 'TWITTER_HANDLE', 'EXTRA_PERSON', 'CREDIT_CARD_NUMBER',
+                        'PHONE_NUMBER', 'EMAIL_ADDRESS', 'US_SSN', 'PERSON']
+
+        no_pii_list = []
+        pii_list = []
+        file_list = scan_files()
+        for f in file_list:
+            txt_list = get_file_text(f)
+            for txt in txt_list:
+                if txt:
+                    results = analyzer.analyze(text=txt, entities=detect_types, language='en')
+                    if results:
+                        pii_list.append(f+':'+txt)
+                        pii_list.append(results)
+                    else:
+                        no_pii_list.append(f+':'+txt)
+
+        print('PII List')
+        for txt in pii_list:
+            print(txt)
+        print('No PII list: ')
+        for txt in no_pii_list:
+            print(txt)
 
     def test_docx(self):
         # test to make sure we can read a docx ok
